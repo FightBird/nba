cce572e0ad9a696d328ee5d2cdb6dad7b177e140
zhangzhenjun <xwp_fullstack@163.com>
2018-12-02 08:18:22 +0800

    图片验证码

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -5,6 +5,7 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SmsCodeView.as_view()),
+    url(r'^sms_codes/$', views.SmsCodeView.as_view()),
     url(r'^usernames/(?P<username>\w+)/count/$', views.UserNameView.as_view()),
     url(r'^mobiles/(?P<mobile>\d+)/count/$', views.MobileView.as_view()),
     url(r'^users/$', views.UsersView.as_view()),
@@ -13,7 +14,8 @@ urlpatterns = [
     url(r'^emails/verification/$', views.VerifyEmailView.as_view()),
     url(r'^browse_histories/$', views.UserBrowsingHistoryView.as_view()),
     url(r'^authorizations/$', views.UserAuthorizeView.as_view()),
-    url(r'^image_code/$', views.ForgorPasswordView.as_view()),
+    url(r'^image_codes/(?P<image_code_id>[\w-]+)/$', views.ImageCodeView.as_view()),
+    url(r'^accounts/(?P<username>\w+)/sms/token/$', views.UserNameView.as_view()),
 
 ]
 
